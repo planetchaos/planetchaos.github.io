22a789c40c2d76d335960acb683f4edf00e78e7f
Laksh Arora <laksharora28@gmail.com>
2021-04-06 06:17:34 +0530

    new section added, new js for scroll, next, previous button

--- js/scroll-text.js
+++ js/scroll-text.js
@@ -1,89 +1,32 @@
-window.addEventListener("wheel", event => {
-    const delta = Math.sign(event.deltaY);
-    console.log(delta);
-    var section = document.getElementById('section').innerHTML;
-    console.log(section);
-    if (delta < 0 && section == 1) {
-        console.log('next');
-        var main = document.getElementsByClassName('main-container')[0];
-        console.log(main);
-        var elements = `
-        <p hidden id="section">0</p>
-        <div><span> a </span></div>
-        <a href="place.html"><div id="place"><span> place </span></div></a>
-        <div><span> to </span></div>
-        <a href="explore.html"><div id="explore"><span> explore </span></div></a>
-        <a href=""><div id="ideas"><span> ideas </span></div></a>
-        <div><span> by </span></div>
-        <a href="https://instagram.com/techedlaksh"><div id="name"><span> @techedlaksh </span></div></a>
-        `;
-        main.innerHTML = elements;
-    } if (delta > 0 && section == 0) {
-        console.log('previous');
-        var main = document.getElementsByClassName('main-container')[0];
-        console.log(main);
-        var elements = `
-        <p hidden id="section">1</p>
-        <div><span> Currently  </span></div>
-        <a href="place.html"><div id="place"><span> experimenting </span></div></a>
-        <div><span> with </span></div>
-        <a href="explore.html"><div id="explore"><span> blah </span></div></a>
-        <a href=""><div id="ideas"><span> blah </span></div></a>
-        <div><span> blah </span></div>
-        <a href="https://instagram.com/techedlaksh"><div id="name"><span> blah </span></div></a>
-        `;
-        main.innerHTML = elements;
+var scrollPos = 0;
+window.addEventListener('scroll', function(){
+    var section = document.getElementById('section');
+    var value = window.scrollY;
+    // var value = (document.body.getBoundingClientRect()).top;
+    if (value < scrollPos && section.innerHTML == 1) {
+        window.scrollTo(0, 0);
+        section.innerHTML = 0;
+        document.getElementsByClassName('scroll')[1].setAttribute("hidden", "");
+        document.getElementsByClassName('scroll')[0].removeAttribute("hidden", "");
+    } else if (value > scrollPos && section.innerHTML == 0) {
+        var s2 = document.getElementsByClassName('item-container2')[0].scrollHeight;
+        window.scrollTo(0, s2);
+        section.innerHTML = 1;
+        document.getElementsByClassName('scroll')[0].setAttribute("hidden", "");
+        document.getElementsByClassName('scroll')[1].removeAttribute("hidden", "");
     }
+    // scrollPos = (document.body.getBoundingClientRect()).top;
+    scrollPos = window.scrollY;
 });
 
-// window.addEventListener("touchstart", touchStart, false);
-// window.addEventListener("touchmove", touchMove, false);
-//
-// var start = {x:0, y:0};
-//
-// function touchStart(event) {
-//     start.x = event.touches[0].pageX;
-//     start.y = event.touches[0].pageY;
-// }
-//
-// function touchMove(event) {
-//     offset = {};
-//
-//     offset.x = start.x - event.touches[0].pageX;
-//     offset.y = start.y - event.touches[0].pageY;
-//
-//     // window.alert(offset.x);
-//     var section = document.getElementById('section').innerHTML;
-//     console.log(section);
-//     if (offset.x > 0 && section == 1) {
-//         console.log('next');
-//         var main = document.getElementsByClassName('main-container')[0];
-//         console.log(main);
-//         var elements = `
-//         <p hidden id="section">0</p>
-//         <div><span> a </span></div>
-//         <a href="place.html"><div id="place"><span> place </span></div></a>
-//         <div><span> to </span></div>
-//         <a href="explore.html"><div id="explore"><span> explore </span></div></a>
-//         <a href=""><div id="ideas"><span> ideas </span></div></a>
-//         <div><span> by </span></div>
-//         <a href="https://instagram.com/techedlaksh"><div id="name"><span> @techedlaksh </span></div></a>
-//         `;
-//         main.innerHTML = elements;
-//     } if (offset.x < -10  && section == 0) {
-//         console.log('previous');
-//         var main = document.getElementsByClassName('main-container')[0];
-//         console.log(main);
-//         var elements = `
-//         <p hidden id="section">1</p>
-//         <div><span> Currently  </span></div>
-//         <a href="place.html"><div id="place"><span> experimenting </span></div></a>
-//         <div><span> with </span></div>
-//         <a href="explore.html"><div id="explore"><span> blah </span></div></a>
-//         <a href=""><div id="ideas"><span> blah </span></div></a>
-//         <div><span> blah </span></div>
-//         <a href="https://instagram.com/techedlaksh"><div id="name"><span> blah </span></div></a>
-//         `;
-//         main.innerHTML = elements;
-//     }
-// }
+document.getElementById('first')
+    .addEventListener("click", function(event) {
+        document.getElementsByClassName('scroll')[0].setAttribute("hidden", "");
+        document.getElementsByClassName('scroll')[1].removeAttribute("hidden", "");
+    });
+
+document.getElementById('second')
+    .addEventListener("click", function(event) {
+        document.getElementsByClassName('scroll')[1].setAttribute("hidden", "");
+        document.getElementsByClassName('scroll')[0].removeAttribute("hidden", "");
+    });
